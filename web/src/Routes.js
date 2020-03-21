f412012484ef784e1504cb2cf9f2abb14fdeef14
Terris Kremer <terris.kremer@gmail.com>
2020-03-21 18:02:34 -0400

    create blog

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,6 +12,18 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/admin/posts" page={PostsPage} name="posts" />
+      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+      <Route
+        path="/admin/posts/{id:Int}/edit"
+        page={EditPostPage}
+        name="editPost"
+      />
+      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+      <Route path="/admin/about" page={AboutPage} name="about" />
+      <Route path="/" page={HomePage} name="home" />
+      <Route path="/contact" page={ContactPage} name="contact" />
+      <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
